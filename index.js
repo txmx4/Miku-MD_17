bba5cd74f6174e213e86359b22c7181e50b493af
txmx4 <123477527+txmx4@users.noreply.github.com>
2023-01-24 17:07:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ console.log(color(figlet.textSync('Miku Bot MD', {
 		whitespaceBreak: true
         }), 'yellow'))
 
-console.log(color('\nHello, I am Fantox, the main developer of this bot.\n\nThanks for using: Miku Bot','aqua'))
+console.log(color('\nHallo, I am Sebastian, the main developer of this bot.\n\nThanks for using: Miku Bot','aqua'))
 console.log(color('\nYou can follow me on GitHub: txmx3','aqua'))
 
     let { version, isLatest } = await fetchLatestBaileysVersion()
     const Miku = MikuConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Mika by: Max_2','Safari','1.0.0'],
+        browser: ['Miku by: Max_2','Safari','1.0.0'],
         auth: state,
         version
     })
@@ -73,7 +73,7 @@ Miku.ev.on('groups.update', async pea => {
        try {
        ppgc = await Miku.profilePictureUrl(pea[0].id, 'image')
        } catch {
-       ppgc = 'https://wallpapercave.com/wp/wp10524580.jpg'
+       ppgc = 'https://wallpapercave.com/wp/wp6299451.jpg'
        }
        let wm_fatih = { url : ppgc }
        if (pea[0].announce == true) {
@@ -92,7 +92,7 @@ Miku.ev.on('groups.update', async pea => {
         try {
         ppgc = await Miku.profilePictureUrl(pea[0].id, 'image')
         } catch {
-        ppgc = 'https://wallpapercave.com/wp/wp10524580.jpg'
+        ppgc = 'https://wallpapercave.com/wp/wp6299451.jpg'
         }
         let wm_fatih = { url : ppgc }
         if (pea[0].announce == true) {
